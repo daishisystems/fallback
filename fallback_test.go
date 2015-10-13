5f886fb72b12178ba169130b492f11111518d83c
Paul Mooney <paul@daishisystems.com>
2015-10-13 15:42:57 +0100

    Added complex fallback test

--- fallback_test.go
+++ fallback_test.go
@@ -66,7 +66,46 @@ func TestSimpleFallback(t *testing.T) {
 	statusCode, err := failing.ExecuteHTTPRequest()
 
 	if err != nil {
-		t.Log("HTTP status code:", statusCode)
+		t.Fatal(err)
+	}
+
+	if statusCode != 200 {
+		t.Fatal("For", "Basic GET",
+			"expected", 200,
+			"got", statusCode)
+	}
+
+	if basicResponse.Text != "OK" || basicResponse.Detail != "Successful HTTP request" {
+		t.Error("For", "Basic GET",
+			"expected", "OK, Successful HTTP request",
+			"got", basicResponse.Text, basicResponse.Detail)
+	}
+}
+
+func TestComplexFallback(t *testing.T) {
+
+	basicResponse := &BasicResponse{}
+	basicError := &BasicError{}
+
+	headers := map[string]string{
+		"Content-Type": "application/json",
+	}
+
+	workingPOST := NewConnection("Working POST", "POST",
+		"http://demo7227109.mockable.io/post-basic", nil, headers,
+		basicResponse, basicError, nil)
+
+	failingPOST := NewConnection("Failing POST", "POST",
+		"http://demo7227109.mockable.io/fail-basic-post", nil, headers,
+		basicResponse, basicError, workingPOST)
+
+	failingGet := NewConnection("Failing GET", "GET",
+		"http://demo7227109.mockable.io/fail-basic", nil, headers,
+		basicResponse, basicError, failingPOST)
+
+	statusCode, err := failingGet.ExecuteHTTPRequest()
+
+	if err != nil {
 		t.Fatal(err)
 	}
 
